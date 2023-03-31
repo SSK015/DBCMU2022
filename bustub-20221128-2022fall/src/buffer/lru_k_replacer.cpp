1b23e6bf857c1f2c54cc694665e06d6118657239
SSK015 <18252677381@163.com>
2023-03-31 16:07:53 +0800

    doing2/2p1

--- bustub-20221128-2022fall/src/buffer/lru_k_replacer.cpp
+++ bustub-20221128-2022fall/src/buffer/lru_k_replacer.cpp
@@ -15,15 +15,91 @@
 namespace bustub {
 
 LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {}
-
-auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool { return false; }
-
+/**
+   * TODO(P1): Add implementation
+   *
+   * @brief Find the frame with largest backward k-distance and evict that frame. Only frames
+   * that are marked as 'evictable' are candidates for eviction.
+   *
+   * A frame with less than k historical references is given +inf as its backward k-distance.
+   * If multiple frames have inf backward k-distance, then evict the frame with the earliest
+   * timestamp overall.
+   *
+   * Successful eviction of a frame should decrement the size of replacer and remove the frame's
+   * access history.
+   *
+   * @param[out] frame_id id of frame that is evicted.
+   * @return true if a frame is evicted successfully, false if no frames can be evicted.
+   */
+auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool { 
+    if (curr_size_ == 0) return false;
+    
+}
+  /**
+   * TODO(P1): Add implementation
+   *
+   * @brief Record the event that the given frame id is accessed at current timestamp.
+   * Create a new entry for access history if frame id has not been seen before.
+   *
+   * If frame id is invalid (ie. larger than replacer_size_), throw an exception. You can
+   * also use BUSTUB_ASSERT to abort the process if frame id is invalid.
+   *
+   * @param frame_id id of frame that received a new access.
+   */
 void LRUKReplacer::RecordAccess(frame_id_t frame_id) {}
+/**
+   * TODO(P1): Add implementation
+   *
+   * @brief Toggle whether a frame is evictable or non-evictable. This function also
+   * controls replacer's size. Note that size is equal to number of evictable entries.
+   *
+   * If a frame was previously evictable and is to be set to non-evictable, then size should
+   * decrement. If a frame was previously non-evictable and is to be set to evictable,
+   * then size should increment.
+   *
+   * If frame id is invalid, throw an exception or abort the process.
+   *
+   * For other scenarios, this function should terminate without modifying anything.
+   *
+   * @param frame_id id of frame whose 'evictable' status will be modified
+   * @param set_evictable whether the given frame is evictable or not
+   */
+void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
+    if (frame_id > replacer_size_) {
+        throw std::exception();
+    }
+    if (Evictable_[frame_id] && set_evictable == false) {
+        curr_size_--;
+        Evictable_[frame_id] = false;
+    } else if (Evictable_[frame_id] == false && set_evictable == true){ 
+        curr_size_++;
+        Evictable_[frame_id] = true;
+    }
+    return ;
+        //Evictable_[frame_id] = set_evictable;
 
-void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {}
-
+}
+  /**
+   * TODO(P1): Add implementation
+   *
+   * @brief Remove an evictable frame from replacer, along with its access history.
+   * This function should also decrement replacer's size if removal is successful.
+   *
+   * Note that this is different from evicting a frame, which always remove the frame
+   * with largest backward k-distance. This function removes specified frame id,
+   * no matter what its backward k-distance is.
+   *
+   * If Remove is called on a non-evictable frame, throw an exception or abort the
+   * process.
+   *
+   * If specified frame is not found, directly return from this function.
+   *
+   * @param frame_id id of frame to be removed
+   */
 void LRUKReplacer::Remove(frame_id_t frame_id) {}
 
-auto LRUKReplacer::Size() -> size_t { return 0; }
+auto LRUKReplacer::Size() -> size_t { 
+    return curr_size_; 
+    }
 
 }  // namespace bustub
