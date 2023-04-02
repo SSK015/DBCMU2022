f73e57f701d7cb5f9fb626f1d8625a21d98db7a9
SSK015 <18252677381@163.com>
2023-04-02 23:07:19 +0800

    doing3/32

--- bustub-20221128-2022fall/src/buffer/buffer_pool_manager_instance.cpp
+++ bustub-20221128-2022fall/src/buffer/buffer_pool_manager_instance.cpp
@@ -41,9 +41,24 @@ BufferPoolManagerInstance::~BufferPoolManagerInstance() {
   delete page_table_;
   delete replacer_;
 }
-
+  /**
+   * TODO(P1): Add implementation
+   *
+   * @brief Create a new page in the buffer pool. Set page_id to the new page's id, or nullptr if all frames
+   * are currently in use and not evictable (in another word, pinned).
+   *
+   * You should pick the replacement frame from either the free list or the replacer (always find from the free list
+   * first), and then call the AllocatePage() method to get a new page id. If the replacement frame has a dirty page,
+   * you should write it back to the disk first. You also need to reset the memory and metadata for the new page.
+   *
+   * Remember to "Pin" the frame by calling replacer.SetEvictable(frame_id, false)
+   * so that the replacer wouldn't evict the frame before the buffer pool manager "Unpin"s it.
+   * Also, remember to record the access history of the frame in the replacer for the lru-k algorithm to work.
+   *
+   * @param[out] page_id id of created page
+   * @return nullptr if no new pages could be created, otherwise pointer to new page
+   */
 auto BufferPoolManagerInstance::NewPgImp(page_id_t *page_id) -> Page * { 
-  
   bool free_page = false;
   for (size_t i = 0; i < pool_size_; ++i) {
     if (pages_[i].GetPinCount() == 0) {
@@ -55,7 +70,31 @@ auto BufferPoolManagerInstance::NewPgImp(page_id_t *page_id) -> Page * {
     return nullptr;
   }
   *page_id = AllocatePage();
+
+  frame_id_t frame_id;
+  if (!free_list_.empty()) {
+    frame_id = free_list_.front();
+    free_list_.pop_front();
+  } else {
+    replacer_->Evict(&frame_id);
+    page_id_t evicted_page_id = pages_[frame_id].GetPageId();
+
+    if (pages_[frame_id].IsDirty()) {
+      disk_manager_->WritePage(evicted_page_id, pages_[frame_id].GetData());
+      pages_[frame_id].is_dirty_ = false;
+    }
+    pages_[frame_id].ResetMemory();
+
+    page_table_->Remove(frame_id);
+
+  }
+  page_table_->Insert(*page_id, frame_id);
+
+  pages_[frame_id].page_id_ = *page_id;
+  pages_[frame_id].pin_count_ = 1;
   
+  replacer_->RecordAccess(frame_id);
+  replacer_->SetEvictable(frame_id, false);
 }
 
 auto BufferPoolManagerInstance::FetchPgImp(page_id_t page_id) -> Page * { return nullptr; }
