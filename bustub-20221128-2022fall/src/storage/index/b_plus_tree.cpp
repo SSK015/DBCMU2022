921a5250505d1497838df047bcff951a3cafed19
SSK015 <18252677381@163.com>
2023-05-14 23:57:51 +0800

    startBPT

--- bustub-20221128-2022fall/src/storage/index/b_plus_tree.cpp
+++ bustub-20221128-2022fall/src/storage/index/b_plus_tree.cpp
@@ -21,7 +21,10 @@ BPLUSTREE_TYPE::BPlusTree(std::string name, BufferPoolManager *buffer_pool_manag
  * Helper function to decide whether current b+tree is empty
  */
 INDEX_TEMPLATE_ARGUMENTS
-auto BPLUSTREE_TYPE::IsEmpty() const -> bool { return true; }
+auto BPLUSTREE_TYPE::IsEmpty() const -> bool { 
+  return root_page_id_ = INVALID_PAGE_ID;
+  //return true;
+}
 /*****************************************************************************
  * SEARCH
  *****************************************************************************/
